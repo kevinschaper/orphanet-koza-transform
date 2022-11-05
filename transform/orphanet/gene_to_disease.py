226cdb24523e325650325492d2e27fd50452aff9
caufieldjh <j.harry.caufield@gmail.com>
2022-11-05 01:08:18 -0400

    Use list of preferred gene sources

--- transform/orphanet/gene_to_disease.py
+++ transform/orphanet/gene_to_disease.py
@@ -3,6 +3,8 @@ import uuid
 from biolink.model import Association, Disease, Gene
 from koza.cli_runner import get_koza_app
 
+PREFERRED_GENE_SOURCES = ["HGNC"]
+
 koza_app = get_koza_app("orphanet_gene_to_disease")
 
 while (row := koza_app.get_row()) is not None:
@@ -16,7 +18,7 @@ while (row := koza_app.get_row()) is not None:
 
         possible_genes = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"]["Gene"]["ExternalReferenceList"][
             "ExternalReference"]
-        possible_hgnc_genes = [gene for gene in possible_genes if gene["Source"] == 'HGNC']
+        possible_hgnc_genes = [gene for gene in possible_genes if gene["Source"] in PREFERRED_GENE_SOURCES]
         assert len(possible_hgnc_genes) == 1
         gene = possible_hgnc_genes[0]
         gene_id = gene["Source"] + ':' + gene["Reference"]
