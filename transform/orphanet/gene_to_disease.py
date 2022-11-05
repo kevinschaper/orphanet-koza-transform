f9f0a3d7c6325e9d5638a8d6f9ee789e58416c79
caufieldjh <j.harry.caufield@gmail.com>
2022-11-05 01:46:55 -0400

    Map Orphanet relation types to Biolink

--- transform/orphanet/gene_to_disease.py
+++ transform/orphanet/gene_to_disease.py
@@ -1,9 +1,24 @@
 import uuid
 
-from biolink.model import Association, Disease, Gene
+from biolink.model import Disease, Gene, Association
 from koza.cli_runner import get_koza_app
 
+# Options here include:
+# Ensembl, Genatlas, HGNC,
+# IUPHAR, OMIM, Reactome
+# Swissprot
 PREFERRED_GENE_SOURCES = ["HGNC"]
+
+RELATION_TYPE_MAP = {"Disease-causing germline mutation(s) in": "biolink:condition_associated_with_gene",
+                     "Disease-causing germline mutation(s) (loss of function) in": "biolink:condition_associated_with_gene",
+                     "Disease-causing germline mutation(s) (gain of function) in":"biolink:condition_associated_with_gene",
+                     "Role in the phenotype of": "biolink:condition_associated_with_gene",
+                     "Major susceptibility factor in": "biolink:condition_associated_with_gene",
+                     "Disease-causing somatic mutation(s) in": "biolink:condition_associated_with_gene",
+                     "Candidate gene tested in": "biolink:related_to",
+                     "Part of a fusion gene in": "biolink:condition_associated_with_gene",
+                     "Biomarker tested in": "biolink:has_biomarker",
+                     }
 
 koza_app = get_koza_app("orphanet_gene_to_disease")
 
@@ -11,33 +26,40 @@ while (row := koza_app.get_row()) is not None:
     try:
         disease_id = "Orphanet:" + row["OrphaCode"]
         disease = Disease(
-            id = disease_id,
-            name = row['Name']['#text'],
-            category = "biolink:Disease"
+            id=disease_id,
+            name=row['Name']['#text'],
+            category="biolink:Disease"
         )
 
         possible_genes = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"]["Gene"]["ExternalReferenceList"][
             "ExternalReference"]
-        possible_hgnc_genes = [gene for gene in possible_genes if gene["Source"] in PREFERRED_GENE_SOURCES]
+        possible_hgnc_genes = [
+            gene for gene in possible_genes if gene["Source"] in PREFERRED_GENE_SOURCES]
         assert len(possible_hgnc_genes) == 1
         gene = possible_hgnc_genes[0]
         gene_id = gene["Source"] + ':' + gene["Reference"]
         gene = Gene(
-            id = gene_id,
-            category = "biolink:Gene"
+            id=gene_id,
+            category="biolink:Gene"
         )
 
         # todo: map the orphanet_association_type to a biolink predicate
-        orphanet_association_type = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"]['DisorderGeneAssociationType']['Name']['#text']
+        orphanet_association_type = row["DisorderGeneAssociationList"][
+            "DisorderGeneAssociation"]['DisorderGeneAssociationType']['Name']['#text']
+        if orphanet_association_type in RELATION_TYPE_MAP:
+            predicate = RELATION_TYPE_MAP[orphanet_association_type]
+        else:
+            predicate = "biolink:related_to"
         association = Association(
-            id = "uuid:" + str(uuid.uuid1()),
-            subject = disease.id,
-            predicate = "biolink:Association",
-            object = gene.id,
+            id="uuid:" + str(uuid.uuid1()),
+            subject=disease.id,
+            predicate=predicate,
+            category="biolink:Association",
+            object=gene.id,
         )
 
         koza_app.write(disease, association, gene)
-    
-    except (TypeError, ValueError) as e:
-        print(f"Invalid entry: {e}. Row contents: {row}")
 
+    except (TypeError, ValueError) as e:
+        row_id = row['@id']
+        print(f'Invalid entry: {e}. See entry {row_id}')
