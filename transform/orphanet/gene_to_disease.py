636c2e28f3674327ecf5e824943ebccca5a5d367
caufieldjh <j.harry.caufield@gmail.com>
2022-11-05 02:50:05 -0400

    Refactor to get first gene ID matching preference list

--- transform/orphanet/gene_to_disease.py
+++ transform/orphanet/gene_to_disease.py
@@ -44,34 +44,38 @@ while (row := koza_app.get_row()) is not None:
         koza_app.write(disease)
 
         # Gene
-        possible_genes = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"][
-            "Gene"
-        ]["ExternalReferenceList"]["ExternalReference"]
-        possible_preferred_genes = [
-            gene for gene in possible_genes if gene["Source"] in PREFERRED_GENE_SOURCES
-        ]
+        # See if there's any overlap between the preferred sources
+        # and the available sources first.
+        all_assoc_genes = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"]
+        for gene in all_assoc_genes:
+            is_preferred_gene = False
+            all_ex_refs = gene["Gene"]["ExternalReferenceList"]["ExternalReference"]
+            for ref in all_ex_refs:
+                if ref['Source'] in PREFERRED_GENE_SOURCES:
+                    source = ref["Source"]
+                    ref = ref["Reference"]
+                    is_preferred_gene = True
+                    break
+            
+            if is_preferred_gene:
+                gene_id = source + ":" + ref
+                gene_obj = Gene(id=gene_id, category="biolink:Gene")
 
-        for gene in possible_preferred_genes:
-            gene_id = gene["Source"] + ":" + gene["Reference"]
-            gene = Gene(id=gene_id, category="biolink:Gene")
+                # Association
+                orphanet_association_type = gene["DisorderGeneAssociationType"]["Name"]["#text"]
+                if orphanet_association_type in RELATION_TYPE_MAP:
+                    predicate = RELATION_TYPE_MAP[orphanet_association_type]
+                else:
+                    predicate = "biolink:related_to"
+                association = Association(
+                    id="uuid:" + str(uuid.uuid1()),
+                    subject=disease.id,
+                    predicate=predicate,
+                    category="biolink:Association",
+                    object=gene_obj.id,
+                )
 
-            # Association
-            orphanet_association_type = row["DisorderGeneAssociationList"][
-                "DisorderGeneAssociation"
-            ]["DisorderGeneAssociationType"]["Name"]["#text"]
-            if orphanet_association_type in RELATION_TYPE_MAP:
-                predicate = RELATION_TYPE_MAP[orphanet_association_type]
-            else:
-                predicate = "biolink:related_to"
-            association = Association(
-                id="uuid:" + str(uuid.uuid1()),
-                subject=disease.id,
-                predicate=predicate,
-                category="biolink:Association",
-                object=gene.id,
-            )
-
-            koza_app.write(association, gene)
+                koza_app.write(association, gene_obj)
 
     except (TypeError, ValueError) as e:
         row_id = row["@id"]
