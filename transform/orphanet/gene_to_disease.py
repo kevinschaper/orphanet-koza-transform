a112d070d48815c29e3844cabc62e4661fa31b54
caufieldjh <j.harry.caufield@gmail.com>
2022-11-05 00:55:44 -0400

    Produce basic nodes/edges

--- transform/orphanet/gene_to_disease.py
+++ transform/orphanet/gene_to_disease.py
@@ -1,4 +1,6 @@
 import uuid
+
+from biolink.model import Association, Disease, Gene
 from koza.cli_runner import get_koza_app
 
 koza_app = get_koza_app("orphanet_gene_to_disease")
@@ -6,21 +8,34 @@ koza_app = get_koza_app("orphanet_gene_to_disease")
 while (row := koza_app.get_row()) is not None:
     try:
         disease_id = "Orphanet:" + row["OrphaCode"]
+        disease = Disease(
+            id = disease_id,
+            category = "biolink:Disease"
+        )
+
+
         possible_genes = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"]["Gene"]["ExternalReferenceList"][
             "ExternalReference"]
         possible_hgnc_genes = [gene for gene in possible_genes if gene["Source"] == 'HGNC']
         assert len(possible_hgnc_genes) == 1
         gene = possible_hgnc_genes[0]
         gene_id = gene["Source"] + ':' + gene["Reference"]
-
-        orphanet_association_type = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"]['DisorderGeneAssociationType']['Name']['#text']
+        gene = Gene(
+            id = gene_id,
+            category = "biolink:Gene"
+        )
 
         # todo: map the orphanet_association_type to a biolink predicate
+        orphanet_association_type = row["DisorderGeneAssociationList"]["DisorderGeneAssociation"]['DisorderGeneAssociationType']['Name']['#text']
+        association = Association(
+            id = "uuid:" + str(uuid.uuid1()),
+            subject = disease.id,
+            predicate = "biolink:Association",
+            object = gene.id,
+        )
 
-        # todo: make an association rather than just printing stuff after sorting out the predicates
-        print(f"{disease_id}\t{orphanet_association_type}\t{gene_id}")
+        koza_app.write(disease, association, gene)
+    
+    except (TypeError, ValueError) as e:
+        print(f"Invalid entry: {e}. Row contents: {row}")
 
-
-    except:
-        # todo: this is way too broad, I just wanted to get the subset where these paths work
-        pass
